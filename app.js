f8d486a151425bb14ecd88cf1a00c0542b80d2db
eugeneboroda <41344500+eugeneboroda@users.noreply.github.com>
2020-09-16 15:51:10 +0200

    RestFul API with Express and Mongo

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
+
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+
+const connect = mongoose.connect(url);
+
+connect.then( db => {
+
+  console.log('Connected succesfully!');
+
+}, err =>{console.log(err)});
 
 var app = express();
 
